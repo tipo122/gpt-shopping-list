4b3c74ec9109d2c2445a06b57e8031809c18ca4d
Keiko Higuchi <47127802+kkoisland@users.noreply.github.com>
2023-05-21 16:37:30 -0700

    change the global background color to #282c34 as default

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './firebase';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
 import MainPage from './pages/MainPage';
 import Login from './components/Login';
 import './App.css';
@@ -11,17 +12,27 @@ function App() {
   const [user] = useAuthState(auth);
 
   return (
-    <Router>
-      {!user && <Navigate to="/login" />}
-      <div className="App">
-        <header className="App-header">
-          <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </header>
-      </div>
-    </Router>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#F09A37',
+          colorBgContainer: '#282c34',
+          colorText: '#fff',
+        },
+      }}
+    >
+      <Router>
+        {!user && <Navigate to="/login" />}
+        <div className="App">
+          <header className="App-header">
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </header>
+        </div>
+      </Router>
+    </ConfigProvider>
   );
 }
 
