73476475148c1d5d14c302e20298f1a4cb695f5c
Akihiro Nishiyama <akih@keroling.net>
2023-05-21 16:57:07 -0700

    add textarea

--- src/components/ChatArea/Chat.tsx
+++ src/components/ChatArea/Chat.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Configuration, OpenAIApi } from 'openai';
+import { Input } from 'antd';
 import ChatList from './ChatList';
 
 interface ChatProps {
@@ -12,27 +13,38 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
+const { TextArea } = Input;
+
 const Chat = (props: ChatProps) => {
+  const [prompt, setPrompt] = useState<string>('');
   const [response, setResponse] = useState<any>();
   const handleClick = async () => {
     const res = await openai.createCompletion({
       model: 'text-davinci-003',
-      prompt: 'hows doing',
+      prompt: prompt,
       temperature: 0.7,
       max_tokens: 256,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
     });
-    setResponse(res);
+    setResponse(res.data.choices[0].text);
   };
 
   return (
     <div>
       <div>Chat Area</div>
-      <button onClick={handleClick}>click me</button>
-      {response && response.text}
+      <hr />
       <ChatList />
+      {response && response}
+      <hr />
+      <TextArea
+        value={prompt}
+        onChange={(e) => {
+          setPrompt(e.target.value);
+        }}
+      ></TextArea>
+      <button onClick={handleClick}>Submit</button>
     </div>
   );
 };
