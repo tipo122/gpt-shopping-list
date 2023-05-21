4f690f8e74ea7afd9e48bb072971f50c94696824
Akihiro Nishiyama <akih@keroling.net>
2023-05-21 14:12:12 -0700

    clean unnecessary lines

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -9,70 +9,10 @@ interface ShoppingListProps {
   children?: React.ReactElement;
 }
 
-const list: Item[] = [
-  {
-    name: 'トイレットペーパー',
-    requiredCount: 2,
-    stockAmount: 0,
-    purchasedAmount: 2,
-    category: '洗剤紙類',
-  },
-  {
-    name: 'しょうゆ',
-    requiredCount: 2,
-    stockAmount: 1,
-    purchasedAmount: 1,
-    category: '調味料',
-  },
-  {
-    name: 'オイスター缶',
-    requiredCount: 3,
-    stockAmount: 2,
-    purchasedAmount: 1,
-    category: 'ごはん用',
-  },
-  { name: '卵', requiredCount: 3, stockAmount: 0, purchasedAmount: 3, category: '冷蔵類' },
-  {
-    name: 'ベーコン',
-    requiredCount: 1,
-    stockAmount: 0,
-    purchasedAmount: 1,
-    category: '保存食',
-  },
-  {
-    name: 'オレンジティー',
-    requiredCount: 1,
-    stockAmount: 1,
-    purchasedAmount: 0,
-    category: '飲みもの',
-  },
-];
-
-const handleClick = async () => {
-  try {
-    list.forEach(async (item) => {
-      const docRef = await addDoc(collection(db, 'items'), item);
-      console.log('Document written with ID: ', docRef.id);
-    });
-  } catch (e) {
-    console.error('Error adding document: ', e);
-  }
-};
-
 const ShoppingList = (props: ShoppingListProps) => {
-  // const [itemList, setItemList] = useState<Item[] | null>(null);
   const [itemsnapshot, loading, error] = useCollection(collection(db, 'items'), {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const docRef = doc(db, 'items');
-  //     const itemList = await getDoc(docRef);
-  //     // setItemList(itemList);
-  //   };
-  //   fetchData();
-  // }, []);
 
   return (
     <div style={{ width: '100%' }}>
@@ -93,7 +33,6 @@ const ShoppingList = (props: ShoppingListProps) => {
             <ShoppingListItem item={item.data() as Item} />
           </li>
         ))}
-      <button onClick={handleClick}>Initialize</button>
     </div>
   );
 };
