11f3a4f4f436c84ff505e6a51eb9b23b9fdf121e
Akihiro Nishiyama <akih@keroling.net>
2023-05-21 15:19:34 -0700

    use table, read items from db

--- src/components/ShoppingList/ShoppingList.tsx
+++ src/components/ShoppingList/ShoppingList.tsx
@@ -2,57 +2,12 @@ import React from 'react';
 import { collection } from 'firebase/firestore';
 import { useCollection } from 'react-firebase-hooks/firestore';
 import { Table } from 'antd';
-import ShoppingListItem from './ShoppingListItem';
 import { Item } from '@models/Item';
 import db from '../../firebase';
 
 interface ShoppingListProps {
   children?: React.ReactElement;
 }
-
-const list: Item[] = [
-  {
-    _id: 0,
-    name: 'トイレットペーパー',
-    requiredCount: 2,
-    stockAmount: 0,
-    purchasedAmount: 2,
-    category: '洗剤紙類',
-  },
-  {
-    _id: 0,
-    name: 'しょうゆ',
-    requiredCount: 2,
-    stockAmount: 1,
-    purchasedAmount: 1,
-    category: '調味料',
-  },
-  {
-    _id: 0,
-    name: 'オイスター缶',
-    requiredCount: 3,
-    stockAmount: 2,
-    purchasedAmount: 1,
-    category: 'ごはん用',
-  },
-  { _id: 0, name: '卵', requiredCount: 3, stockAmount: 0, purchasedAmount: 3, category: '冷蔵類' },
-  {
-    _id: 0,
-    name: 'ベーコン',
-    requiredCount: 1,
-    stockAmount: 0,
-    purchasedAmount: 1,
-    category: '保存食',
-  },
-  {
-    _id: 0,
-    name: 'オレンジティー',
-    requiredCount: 1,
-    stockAmount: 1,
-    purchasedAmount: 0,
-    category: '飲みもの',
-  },
-];
 
 const columns = [
   {
@@ -86,29 +41,14 @@ const ShoppingList = (props: ShoppingListProps) => {
   const [itemsnapshot, loading, error] = useCollection(collection(db, 'items'), {
     snapshotListenOptions: { includeMetadataChanges: true },
   });
-  return (
-    <Table dataSource={list} columns={columns} />
 
-    // <div style={{ width: '100%' }}>
-    //   Shopping List
-    //   {error && <strong>Error: {JSON.stringify(error)}</strong>}
-    //   {loading && <span>Collection: Loading...</span>}
-    //   {itemsnapshot &&
-    //     itemsnapshot.docs.map((item, index) => (
-    //       <li
-    //         key={index}
-    //         style={{
-    //           display: 'flex',
-    //           alignItems: 'center',
-    //           justifyContent: 'space-between',
-    //           flexWrap: 'nowrap',
-    //         }}
-    //       >
-    //         <ShoppingListItem item={item.data() as Item} />
-    //       </li>
-    //     ))}
-    // </div>
-  );
+  const itemlist: Item[] | undefined = itemsnapshot
+    ? itemsnapshot.docs.map((item) => {
+        return item.data() as Item;
+      })
+    : undefined;
+
+  return <Table dataSource={itemlist} columns={columns} />;
 };
 
 export default ShoppingList;
